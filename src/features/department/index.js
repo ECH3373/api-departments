c03a6575fb842c477239637bffc2325462854b05
Eduardo Cortés Herrera <eduardocortes3373@gmail.com>
2025-03-26 12:53:24 -0600

    Index department feature complete

--- src/features/department/index.js
+++ src/features/department/index.js
@@ -1,9 +1,11 @@
 import { controller } from './department.controller.js';
 import { model } from './department.model.js';
+import { query } from './department.query.js';
 import { router } from './department.router.js';
 
 export const department = {
   controller,
   model,
+  query,
   router,
 };
