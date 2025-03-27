458ad234533c239a6b842a085d80660e0a423e34
Eduardo Cortés Herrera <eduardocortes3373@gmail.com>
2025-03-27 16:05:46 -0600

    Fix pagination meta

--- src/services/crud.js
+++ src/services/crud.js
@@ -3,7 +3,8 @@ import { constrainedMemory } from 'process';
 const index = async ({ model, params = {} }) => {
   const data = await model.find(params.query).limit(params.cursor.limit).skip(params.cursor.skip).sort(params.cursor.sort);
   const total = await model.countDocuments(params.query);
-  const meta = { pagination: { page: 1, pages: Math.ceil(total / params.cursor.limit), total } };
+  const page = Math.floor(params.cursor.skip / params.cursor.limit) + 1;
+  const meta = { pagination: { page, pages: Math.ceil(total / params.cursor.limit), total } };
   return { data, meta };
 };
 
